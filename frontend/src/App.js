65fc11db6f1327da62a4ce66e1e9d5dadf001cdb
Utkarsharma7 <utkarsh22sharma1@gmail.com>
2025-03-06 05:02:50 +0530

    Updated the code base with the complete code

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DiffiScore from './components/DiffiScore';
+
+function App() {
+  return (
+    <div className="App">
+      <DiffiScore />
+    </div>
+  );
+}
+
+export default App;
